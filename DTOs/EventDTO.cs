d33127a7afc5610765698fb042fde49d0e18ed1b
i.surkov <i.surkov@innopolis.ru>
2026-04-26 11:09:17 +0500

    Fixes for event validation

--- DTOs/EventDTO.cs
+++ DTOs/EventDTO.cs
@@ -4,19 +4,26 @@ namespace WebTraining.DTOs;
 
 public class EventDto : IValidatableObject
 {
+    [Required]
     public int Id { get; set; }
+    
     [Required(AllowEmptyStrings = true, ErrorMessage = "Заголовок обязателен для заполнения")]
+    [StringLength(200, MinimumLength = 1,
+        ErrorMessage = "Заголовок события должен быть от 1 до 200 символов")]
     public required string Title { get; set; }
+    
     public string? Description { get; set; }
-    [Required]
+    
+    [Required(AllowEmptyStrings = true, ErrorMessage = "Дата начала обязательна к заполнению")]
     public DateTime StartAt { get; set; }
-    [Required]
+    
+    [Required(AllowEmptyStrings = true, ErrorMessage = "Дата окончания обязательна к заполнению")]
     public DateTime EndAt { get; set; }
 
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
         if (EndAt <= StartAt)
-            yield return new ValidationResult("Дата окончания должна быть после даты начала");
+            yield return new ValidationResult("Дата окончания должна быть позже даты начала");
     }
 }
